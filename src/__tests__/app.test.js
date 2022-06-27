3bfbf4badd88b92b72fd9fe831a10830ba2f4089
c3c1l1a <cesswangumukima@gmail.com>
2022-06-27 21:02:07 +0800

    Fix linter errors

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import testFunction from '../modules/exampleModule';
+import testFunction from '../modules/exampleModule.js';
 
 describe('Making sure it works', () => {
   it('should sum 1 plus 1', () => {
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './css/style.css';
 import './index.html';
 
 import exampleModule from './modules/exampleModule.js';
+
 exampleModule();
