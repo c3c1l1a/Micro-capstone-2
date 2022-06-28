44fcd9dafb36727a57b22c3fb0fe72f84ddedb37
t-bello7 <bello4aus@gmail.com>
2022-06-28 14:06:10 +0100

    Add comment pop-up functionality - refactor comments link to comment icon - create the comment pop up structure using dialog - add the close and open functionality to comment modal - add style to comment-modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import './assets/css/style.css';
 import './index.html';
 
-// import exampleModule from './modules/exampleModule.js';
+const commentBtns = document.querySelectorAll('.comments');
+const commentModal = document.querySelector('.comment-modal');
+const closeComment = document.querySelector('.close-modal');
 
-// exampleModule();
+if (typeof commentModal.showModal !== 'function') {
+  commentModal.hidden = true;
+  /* a fallback script to allow this dialog/form to function
+       for legacy browsers that do not support <dialog>
+       could be provided here.
+    */
+}
+
+commentBtns.forEach((commentBtn) => {
+  commentBtn.addEventListener('click', () => {
+    commentModal.show();
+  });
+});
+
+closeComment.addEventListener('click', () => {
+  commentModal.close();
+});
---
+++ src/modules/involvement.js
@@ -0,0 +1,5 @@
+function testFunction(input) {
+  return input;
+}
+
+export default testFunction;
